76a40cea2e64a0a4a568444d0b0e168e6ed24169
Abegailtaitano <liagebat@gmail..com>
2023-06-01 13:23:11 -0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart } from './components';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  }
+
+  useEffect(() => {
+      fetchProducts();
+      fetchCart();
+    }, []);
+
+console.log(cart);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {/*<Products products={products} onAddToCart={handleAddToCart}/> */}
+        <Navbar totalItems={cart.total_items}/>
+        <Cart cart={cart} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/storelogo.png'; 
+import useStyles from './styles';
+
+const Navbar = ({ totalItems }) => {
+    const classes = useStyles(); 
+    return (
+        <>
+        <AppBar position="fixed" className={classes.appBar} color="inherit">
+            <Toolbar>
+                <Typography variant="h6" className={classes.title} color="inherit">
+                    <img src={logo} alt="store" height="25px" className={classes.image} /> 
+                E. Commerce Store 
+                </Typography>
+                <div className={classes.grow} />
+                <div className={classes.button}>
+                <IconButton aria-label="Show cart items" color="inherit">
+                    <Badge badgeContent={totalItems} color="secondary">
+                        <ShoppingCart />
+                    </Badge>
+                    </IconButton>
+                    </div>
+            </Toolbar>
+        </AppBar>
+     </>
+    )
+}
+
+export default Navbar
