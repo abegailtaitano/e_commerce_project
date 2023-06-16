9cb3eb1fd4e33fbbffd1fc40e6f2d6b36222c111
Jesse Cooper <jcooper@stridenyc.com>
2023-06-16 16:19:47 -0500

    Fix commercejs issue

--- src/App.js
+++ src/App.js
@@ -4,40 +4,54 @@ import { Products, Navbar, Cart } from './components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({});
+
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
+      console.log('Fetch Products...')
     const { data } = await commerce.products.list();
-
+    console.log(data)
     setProducts(data);
   }
 
   const fetchCart = async () => {
-    setCart(await commerce.cart.retrieve());
+      console.log('Fetch cart...')
+      const cart = await commerce.cart.retrieve()
+      console.log(cart)
+      setCart(cart);
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const { cart } = await commerce.cart.add(productId, quantity);
-   
-    setCart(cart);
+      console.log('Handle cart....')
+      console.log('productId: '+productId)
+      console.log('quantity: '+quantity)
+      const cart  = await commerce.cart.add(productId, quantity);
+      console.log(cart)
+      setCart(cart);
   }
 
   const handleUpdateCartQty = async (productId, quantity) => {
-    const { cart } = await commerce.cart.update(productId, {quantity});
-    
+      console.log('Update Cart Quantity...')
+      console.log(productId)
+      console.log(quantity)
+    const cart   = await commerce.cart.update(productId, {quantity});
+    console.log('Cart in handle update: '+cart)
     setCart(cart)
   }
 
   const handleRemoveFromCart = async (productId) => {
-      const { cart } = await commerce.cart.remove(productId);
-      
+      console.log('Remove from cart...')
+      console.log(productId)
+      const  cart   = await commerce.cart.remove(productId);
+      console.log(cart)
       setCart(cart);
   }
 
   const handleEmptyCart = async () => {
-    const { cart } = await commerce.cart.empty();
-    
+      console.log('Empty cart...')
+    const  cart   = await commerce.cart.empty();
+    console.log(cart)
     setCart(cart);
   }
 
@@ -52,7 +66,7 @@ const App = () => {
             <Navbar totalItems={cart.total_items} />
             <Switch>
               <Route exact path="/">
-                  <Products products={products} onAddToCart={handleAddToCart} /> 
+                  <Products products={products} onAddToCart={handleAddToCart} />
                   </Route>
               <Route exact path="/cart">
                   <Cart
